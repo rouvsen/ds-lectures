b23f3504574682e282e687c114055117c7ba81a7
rovshan.maharramov <rouvsen@gmail.com>
2024-03-12 17:46:27 +0400

    add function2 for find duplicated words

--- src/main/java/org/rouvsen/example/algo/FindFunctions.java
+++ src/main/java/org/rouvsen/example/algo/FindFunctions.java
@@ -1,14 +1,14 @@
 package org.rouvsen.example.algo;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FindFunctions {
     public static void main(String[] args) {
-        String[] arr = {"alex", "suarez", "ronaldo", "messi", "suarez", "ronaldo"};
-        findDuplicatedWords(arr);
+        String sentence = "alex suarez ronaldo messi suarez ronaldo";
+//        findDuplicatedWords(arr);
+        findDuplicatedWords2(sentence);
     }
     public static void findDuplicatedWords(String[] arr) {
         Set<String> setList = new HashSet<>();
@@ -18,5 +18,16 @@ public class FindFunctions {
             else duplicatedListOfWords.add(el);
         }
         System.out.println(duplicatedListOfWords);
+    }
+    public static void findDuplicatedWords2(String sentence) {
+        List<String> list = new ArrayList<>();
+        Map<String, Long> collected = Arrays.stream(sentence.split(" ")).collect(
+                Collectors.groupingBy(Function.identity(), Collectors.counting())
+        );
+        for(Map.Entry<String, Long> entry : collected.entrySet()) {
+            if (entry.getValue() > 1)
+                list.add(entry.getKey());
+        }
+        System.out.printf("%nDuplicated words are: %s%n", list);
     }
 }
